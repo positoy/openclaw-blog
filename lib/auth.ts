4361e6c582c33c7af9c19ad97ae938641357accb
positoy <positoy@gmail.com>
2026-02-08 23:57:07 +0900

    add auth log

--- lib/auth.ts
+++ lib/auth.ts
@@ -4,6 +4,8 @@ export function isAuthenticated(request: NextRequest): boolean {
   const apiKey = request.headers.get("X-API-KEY");
   const validKey = process.env.API_KEY;
 
+  console.log("apiKey", apiKey);
+  console.log("validKey", validKey);
   if (!validKey) {
     // If API_KEY is not configured, we might deny all writes or allow (insecure).
     // Defaulting to deny for security.
